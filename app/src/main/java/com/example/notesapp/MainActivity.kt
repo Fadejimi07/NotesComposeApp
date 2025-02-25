b3b05fb66cb61735f08f06f9340dd5438fedb421
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-25 00:49:47 -0600

    Display all notes from db

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -4,21 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.notesapp.repository.NotesRepository
+import com.example.notesapp.roomdb.NoteDb
 import com.example.notesapp.ui.theme.NotesAppTheme
+import com.example.notesapp.viewmodel.NoteViewModel
+import com.example.notesapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Room Db
+        val database = NoteDb.getInstance(applicationContext)
+
+        // Repository
+        val repository = NotesRepository(database.notesDao)
+
+        // viewmodel factory
+        val viewMokdelFactory = NoteViewModelFactory(repository)
+
+        // ViewModel
+        val viewModel = ViewModelProvider(
+            this, viewMokdelFactory
+        )[NoteViewModel::class.java]
+        viewModel.getAllNotes()
+
         setContent {
             NotesAppTheme {
+
+                // Display All Records in Room DB
+                val notes by viewModel.allNotes.collectAsStateWithLifecycle()
 
             }
         }
