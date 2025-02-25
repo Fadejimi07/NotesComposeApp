48bb09ffe80e676313ccf4ec02b6acd4f8be2e14
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-24 23:35:43 -0600

    Create the note entity

--- app/src/main/java/com/example/notesapp/roomdb/Note.kt
+++ app/src/main/java/com/example/notesapp/roomdb/Note.kt
@@ -1,5 +1,14 @@
 package com.example.notesapp.roomdb
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-class Note {
-}
+@Entity(tableName = "notes_table")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val color: Int // store color as an ARGB Integer
+)
