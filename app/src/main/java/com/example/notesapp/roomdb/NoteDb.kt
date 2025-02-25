2b275ecaf8ea105f24b0200b67443ed5c81629c1
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-02-24 23:56:04 -0600

    Creating the database using singleton pattern

--- app/src/main/java/com/example/notesapp/roomdb/NoteDb.kt
+++ app/src/main/java/com/example/notesapp/roomdb/NoteDb.kt
@@ -1,13 +1,33 @@
 package com.example.notesapp.roomdb
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [Note::class], version = 1)
-abstract class NoteDb {
-    var instance = Room.databaseBuilder(
-        context = context.applicationContext,
-        NoteDb::class.java,
-        "notes_db"
-    ).build()
+abstract class NoteDb : RoomDatabase() {
+    abstract val notesDao: NoteDao
+
+
+    companion object {
+
+        @Volatile // Volatie prevents any possible race conditions in multithreading situations
+        private var INSTANCE: NoteDb? = null
+
+        fun getInstance(context: Context): NoteDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context = context.applicationContext,
+                        NoteDb::class.java,
+                        "notes_db"
+                    ).build()
+                }
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
